c6d1e270587df2d4130c3514782e677d64e8d21a
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-06 20:31:31 -0300

    feat: add and configure chakra

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
@@ -6,5 +7,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
+  <ChakraProvider>
     <App />
+  </ChakraProvider>
 );
