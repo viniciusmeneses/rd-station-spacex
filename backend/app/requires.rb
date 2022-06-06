601ee607249cdb7f59b71d36b9b97fd3aa89493e
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-06 20:14:01 -0300

    feat: configure sinatra and web server

---
+++ backend/app/requires.rb
@@ -0,0 +1,9 @@
+require "require_all"
+
+require_all "app/domain/models"
+require_all "app/domain/use_cases"
+
+require_all "app/repositories"
+require_all "app/routes"
+
+require_all "lib"
---
+++ backend/app/server.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require "sinatra/namespace"
+require "sinatra/json"
+require "sinatra/cross_origin"
+
+require "sinatra/reloader" if development?
+
+require_relative "requires"
+
+configure do
+  enable :cross_origin
+end
