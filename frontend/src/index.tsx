b53d95a39d42258ac48d55a51625e37f049109aa
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-06 22:34:57 -0300

    feat: configure react-query

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,27 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { App } from "./App";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </QueryClientProvider>
 );
