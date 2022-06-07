30e61019a64e1380811e948207515695f7ab2c96
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-06 22:33:53 -0300

    feat: sort launches by date

--- backend/app/domain/use_cases/get_previous_launches_use_case.rb
+++ backend/app/domain/use_cases/get_previous_launches_use_case.rb
@@ -2,6 +2,6 @@ class GetPreviousLaunchUseCase
   include Inject["launches_repository"]
 
   def execute
-    launches_repository.previous
+    launches_repository.previous.sort_by(&:date).reverse
   end
 end
--- backend/app/domain/use_cases/get_upcoming_launches_use_case.rb
+++ backend/app/domain/use_cases/get_upcoming_launches_use_case.rb
@@ -2,6 +2,6 @@ class GetUpcomingLaunchUseCase
   include Inject["launches_repository"]
 
   def execute
-    launches_repository.upcoming
+    launches_repository.upcoming.sort_by(&:date)
   end
 end
