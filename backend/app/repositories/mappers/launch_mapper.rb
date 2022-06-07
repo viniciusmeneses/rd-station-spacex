1bb4bd45467d8c176e259d09788f68e0931cbe95
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-06 22:34:21 -0300

    fix: do not return success equal to false if is not launched yet

--- backend/app/repositories/mappers/launch_mapper.rb
+++ backend/app/repositories/mappers/launch_mapper.rb
@@ -6,7 +6,7 @@ class LaunchMapper
       details: json["details"],
       flight_number: json["flight_number"],
       date: json["date_utc"] ? Time.iso8601(json["date_utc"]) : nil,
-      success: json["success"] || false,
+      success: json["success"],
       image_url: json["links"]&.dig("patch", "small")
     )
   end
